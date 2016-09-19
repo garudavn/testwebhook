0f6af379da2b74a1726e2323500468afbefa982a
garudavn <hungnt2507@gmail.com>
2016-09-19 17:20:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,3 @@
-from urllib.parse import urlencode
-from urllib.request import Request, urlopen
-
-url = 'https://valuation.homify.com.vn/RealEstatePriceService/landprice'
-post_fields = {'cuoi':'','dau':'','diadiem':'HN','dientich':'80','duong':'Cầu Giấy','loaiBDS':'D','loaiduong':'D','mattien':'','nam':'2016','phuong':'Quan Hoa','quan':'Cầu Giấy'}
-request = Request(url, urlencode(post_fields).encode())
-json = urlopen(request).read().decode()
-print(json)
-
 # movies.py
 
 from flask import Flask, render_template
